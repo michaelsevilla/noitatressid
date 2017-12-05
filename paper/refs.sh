290ab95e7b1c13d9d8281fe116f2c66df6a86113
Michael Sevilla <mikesevilla3@gmail.com>
2017-12-05 10:11:47 -0800

    relatedwork: finish scoping data IO
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

---
+++ paper/refs.sh
@@ -0,0 +1,14 @@
+for f in \
+  "https://www.usenix.org/legacy/events/fast02/full_papers/schmuck/schmuck.pdf" \
+  "https://www.usenix.org/legacy/events/fast08/tech/full_papers/welch/welch_html/" \
+  "http://wiki.old.lustre.org/images/d/da/Understanding_Lustre_Filesystem_Internals.pdf" \
+  "http://docs.orangefs.com/v_2_9/index.htm" \
+  "http://www.pdl.cmu.edu/PDL-FTP/SelfStar/usenix10.pdf" \
+  "http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.528.7565&rep=rep1&type=pdf" \
+  "https://hal.archives-ouvertes.fr/hal-00789086/document" \
+  "https://hadoop.apache.org/docs/r2.4.1/hadoop-project-dist/hadoop-hdfs/CentralizedCacheManagement.html" \
+  "http://ieeexplore.ieee.org/document/4228023/#full-text-section" \
+  "https://dl.acm.org/citation.cfm?id=1362659" \
+; do
+  open $f
+done
